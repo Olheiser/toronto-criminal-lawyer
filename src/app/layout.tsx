78a6a764929f861d5263e9c0bf26f8d67081b05a
Olheiser <tanner.olheiser@gmail.com>
2024-12-11 09:57:08 -0600

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import CalendlyWidget from "./components/CalendlyButton";
+import Script from "next/script"; // Import Script from Next.js
 //import CalendlyPopupButton from "./components/CalendlyPopupButton";
 import "./globals.css"
 import type { Metadata } from "next";
@@ -20,6 +21,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-653876065"
+          strategy="afterInteractive" // Ensures script runs after page load
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive" // Runs after the page becomes interactive
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-653876065');
+            `,
+          }}
+        />
+      </head>
       <body id="root">
         <Header />
          {/* <PageHeader title="About" /> */}
