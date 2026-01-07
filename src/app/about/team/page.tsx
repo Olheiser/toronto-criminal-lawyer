c57ce27c959d35b429034b641b48a4066b475496
Olheiser <tanner.olheiser@gmail.com>
2026-01-07 12:40:50 -0600

    team page update

--- src/app/about/team/page.tsx
+++ src/app/about/team/page.tsx
@@ -6,9 +6,7 @@ import PageHeader from "../../components/PageHeader";
 
 import NicholasRobinson from "../../../../public/nicholas-robinson-criminal-lawyer.webp";
 import TannerOlheiser from "../../../../public/tanner-olheiser.webp";
-import BaileyJohnson from "../../../../public/bailey-johnson.webp";
 import AlexanderSurgenor from "../../../../public/alexander-surgenor-criminal-lawyer.webp";
-import ChrisMacleod from "../../../../public/chris-macleod-criminal-lawyer.webp";
 import MariiaTsyliuryk from "../../../../public/mariia-tsyliuryk.webp";
 import GeorgeBalabanian from "../../../../public/george-balabanian-criminal-lawyer.jpg"
 import HazelWood from "../../../../public/hazel-wood.avif";
@@ -38,7 +36,7 @@ export default function Page() {
                         />
                         <h2 className={styles.teamHeading}>Nicholas Robinson</h2>
                         <h3 className={styles.teamSubheading}>Criminal Defence Lawyer</h3>
-                        <p className={styles.teamCopy}>A proud member of both the Law Society of Saskatchewan and the Law Society of Upper Canada, Nicholas graduated from McGill University’s Faculty of Law in 2008, earning Bachelor degrees in both Civil and Common Laws. He was called to the Bar of Saskatchewan in 2009 and to the Bar of Ontario in 2010. In 2014, he expanded his practice by opening an office in Regina, Saskatchewan.</p>
+                        <p className={styles.teamCopy}>A proud member of both the Law Society of Saskatchewan and the Law Society of Upper Canada, Nicholas graduated from McGill University&apos;s Faculty of Law in 2008, earning Bachelor degrees in both Civil and Common Laws. He was called to the Bar of Saskatchewan in 2009 and to the Bar of Ontario in 2010. In 2014, he expanded his practice by opening an office in Regina, Saskatchewan.</p>
                     </article>
 
                     <article className={styles.teamCard}>
@@ -64,7 +62,7 @@ export default function Page() {
                         />
                         <h2 className={styles.teamHeading}>George Balabanian</h2>
                         <h3 className={styles.teamSubheading}>Criminal Defence Lawyer</h3>
-                        <p className={styles.teamCopy}>George was born and raised in Montreal and was called to the bar in Saskatchewan in 2013 and British Columbia in 2015. He holds two law degrees and a Ph.D. in historical linguistics from the University of Pennsylvania, where he also lectured on Western Armenian. Before focusing on criminal law, George’s practice centered on class actions. A polyglot and avid hiker, he has explored dozens of countries, 45 U.S. states, and 9 Canadian provinces. In his spare time, Dr. Balabanian writes scholarly articles and stays engaged with research.</p>
+                        <p className={styles.teamCopy}>George was born and raised in Montreal and was called to the bar in Saskatchewan in 2013 and British Columbia in 2015. He holds two law degrees and a Ph.D. in historical linguistics from the University of Pennsylvania, where he also lectured on Western Armenian. Before focusing on criminal law, George&apos;s practice centered on class actions. A polyglot and avid hiker, he has explored dozens of countries, 45 U.S. states, and 9 Canadian provinces. In his spare time, Dr. Balabanian writes scholarly articles and stays engaged with research.</p>
                     </article>
 
                     <article className={styles.teamCard}>
@@ -72,7 +70,7 @@ export default function Page() {
                             alt="Hazel Wood, Articling Student"
                             width={300}
                             height={206}
-                            src={ChrisMacleod}
+                            src={HazelWood}
                             className={styles.teamPhoto}
                         />
                         <h2 className={styles.teamHeading}>Hazel Wood 木柔雅</h2>
@@ -90,7 +88,7 @@ export default function Page() {
                         />
                         <h2 className={styles.teamHeading}>Mariia Tsyliuryk</h2>
                         <h3 className={styles.teamSubheading}>Office Manager & Executive Assistant</h3>
-                        <p className={styles.teamCopy}>Mariia Tsyliuryk, an Executive Assistant with a Master’s Degree from the Kyiv National Academy of Internal Affairs of Ukraine, has a decade of experience as an Assistant Judge and has expanded her legal expertise as a lawyer in 2022. Mariia’s wealth of knowledge in legal affairs strengthens the operational capabilities of Nicholas Robinson’s Criminal Defence practice.</p>
+                        <p className={styles.teamCopy}>Mariia Tsyliuryk, an Executive Assistant with a Master&apos;s Degree from the Kyiv National Academy of Internal Affairs of Ukraine, has a decade of experience as an Assistant Judge and has expanded her legal expertise as a lawyer in 2022. Mariia&apos;s wealth of knowledge in legal affairs strengthens the operational capabilities of Nicholas Robinson&apos;s Criminal Defence practice.</p>
                     </article>
 
                     <article className={styles.teamCard}>
@@ -103,7 +101,7 @@ export default function Page() {
                         />
                         <h2 className={styles.teamHeading}>Tanner Olheiser</h2>
                         <h3 className={styles.teamSubheading}>Marketing Manager</h3>
-                        <p className={styles.teamCopy}>Tanner Olheiser, with a diploma in Interactive Design & Technology from Saskatchewan Polytechnic, previously worked as a Business Intelligence Assistant at sMedia. He currently leverages his knowledge of internet marketing and analytics to advance Nicholas Robinson’s marketing strategies and enhance brand awareness.</p>
+                        <p className={styles.teamCopy}>Tanner Olheiser, with a diploma in Interactive Design & Technology from Saskatchewan Polytechnic, previously worked as a Business Intelligence Assistant at sMedia. He currently leverages his knowledge of internet marketing and analytics to advance Nicholas Robinson&apos;s marketing strategies and enhance brand awareness.</p>
                     </article>
 
                     <article className={styles.teamCard}>
@@ -111,12 +109,12 @@ export default function Page() {
                             alt="Yosef Asmat, Criminal Law Legal Draftsman & Marketing Expert"
                             width={300}
                             height={206}
-                            src={TannerOlheiser}
+                            src={YosefAsmat}
                             className={styles.teamPhoto}
                         />
                         <h2 className={styles.teamHeading}>Yosef Asmat</h2>
                         <h3 className={styles.teamSubheading}>Legal Draftsman & Marketing Expert</h3>
-                        <p className={styles.teamCopy}>Yosef completed a legal clinic at a full-service corporate law firm in downtown Toronto, and worked as an intern for a class action law firm. He also brings over five years of business development experience in the consulting, SAAS, and home services industries. Drawing on his background in sales, Yosef he currently advances the firm's client acquisition strategy whilst also contributing to case management and legal research.</p>
+                        <p className={styles.teamCopy}>Yosef completed a legal clinic at a full-service corporate law firm in downtown Toronto, and worked as an intern for a class action law firm. He also brings over five years of business development experience in the consulting, SAAS, and home services industries. Drawing on his background in sales, Yosef he currently advances the firm&apos;s client acquisition strategy whilst also contributing to case management and legal research.</p>
                     </article>
 
                     <article className={styles.teamCard}>
@@ -124,12 +122,12 @@ export default function Page() {
                             alt="Maksym Kachurovsky, Criminal Law Receivables & Finance Manager"
                             width={300}
                             height={206}
-                            src={TannerOlheiser}
+                            src={MaksymKachurovsky}
                             className={styles.teamPhoto}
                         />
                         <h2 className={styles.teamHeading}>Maksym Kachurovsky</h2>
                         <h3 className={styles.teamSubheading}>Receivables & Finance Manager</h3>
-                        <p className={styles.teamCopy}>Maksym Kachurovskyi is pursuing a Bachelor of Commerce (Honors) degree with a concentration in International Business from Carleton University’s Sprott School of Business. Fluent in English, Ukrainian, and Russian, Maksym brings a global perspective and strong financial acumen to his work. He contributes through careful financial reporting and supporting operational planning, helping strengthen the firm’s fiscal management and efficiency.</p>
+                        <p className={styles.teamCopy}>Maksym Kachurovskyi is pursuing a Bachelor of Commerce (Honors) degree with a concentration in International Business from Carleton University&apos;s Sprott School of Business. Fluent in English, Ukrainian, and Russian, Maksym brings a global perspective and strong financial acumen to his work. He contributes through careful financial reporting and supporting operational planning, helping strengthen the firm&apos;s fiscal management and efficiency.</p>
                     </article>
                     
 
